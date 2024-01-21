eb777915a0ec5a97c40341a2d2b03772cf34dd41
Jay Kapoor <jkap86@gmail.com>
2024-01-20 19:07:40 -0500

    ***

--- server/app/controllers/trade.controller.js
+++ server/app/controllers/trade.controller.js
@@ -1,4 +1,4 @@
-'use strict'
+"use strict";
 const db = require("../models");
 const User = db.users;
 const Trade = db.trades;
@@ -6,188 +6,182 @@ const League = db.leagues;
 const Op = db.Sequelize.Op;
 const sequelize = db.sequelize;
 
-
 exports.leaguemate = async (req, res) => {
+  let filters = [];
 
-    let filters = [];
+  if (req.body.manager) {
+    filters.push({
+      managers: {
+        [Op.contains]: [req.body.manager],
+      },
+    });
+  }
 
-    if (req.body.manager) {
-        filters.push({
-            managers: {
-                [Op.contains]: [req.body.manager]
-            }
-        })
+  if (req.body.player) {
+    if (req.body.player.includes(".")) {
+      const pick_split = req.body.player.split(" ");
+      const season = pick_split[0];
+      const round = parseInt(pick_split[1]?.split(".")[0]);
+      const order =
+        parseInt(season) === parseInt(new Date().getFullYear())
+          ? parseInt(pick_split[1]?.split(".")[1])
+          : null;
+
+      filters.push({
+        players: {
+          [Op.contains]: [`${season} ${round}.${order}`],
+        },
+      });
+    } else {
+      filters.push({
+        players: {
+          [Op.contains]: [req.body.player],
+        },
+      });
     }
+  }
 
-    if (req.body.player) {
-        if (req.body.player.includes('.')) {
-            const pick_split = req.body.player.split(' ')
-            const season = pick_split[0]
-            const round = parseInt(pick_split[1]?.split('.')[0])
-            const order = parseInt(season) === parseInt(new Date().getFullYear()) ? parseInt(pick_split[1]?.split('.')[1]) : null
+  if (req.body.trade_date) {
+    filters.push({
+      status_updated: {
+        [Op.and]: [
+          { [Op.lt]: new Date(req.body.trade_date).getTime() },
+          {
+            [Op.gt]: new Date(
+              new Date(req.body.trade_date) - 7 * 24 * 60 * 60 * 1000
+            ).getTime(),
+          },
+        ],
+      },
+    });
+  }
 
-            filters.push({
-                players: {
-                    [Op.contains]: [`${season} ${round}.${order}`]
-                }
+  let lmTrades;
 
-            })
-        } else {
-            filters.push({
-                players: {
-                    [Op.contains]: [req.body.player]
-                }
+  try {
+    lmTrades = await Trade.findAndCountAll({
+      order: [["status_updated", "DESC"]],
+      offset: 0,
+      limit: 50,
+      where: { [Op.and]: filters },
+      attributes: [
+        "transaction_id",
+        "status_updated",
+        "rosters",
+        "managers",
+        "adds",
+        "drops",
+        "draft_picks",
+        "leagueLeagueId",
+      ],
+      include: [
+        {
+          model: League,
+          attributes: [
+            "league_id",
+            "name",
+            "avatar",
+            "roster_positions",
+            "scoring_settings",
+            "settings",
+          ],
+        },
+      ],
+      group: ["trade.transaction_id", "league.league_id"],
+      raw: true,
+    });
 
-            })
-        }
-    }
+    const trades_to_send = {
+      rows: lmTrades.rows,
+      count: lmTrades?.count?.length,
+    };
 
-    if (req.body.trade_date) {
-        filters.push({
-            status_updated: {
-                [Op.and]: [
-                    { [Op.lt]: new Date(req.body.trade_date).getTime() },
-                    { [Op.gt]: new Date(new Date(req.body.trade_date) - 7 * 24 * 60 * 60 * 1000).getTime() }
-                ]
-            }
-        })
-    }
-
-    let lmTrades;
-
-    try {
-        
-        lmTrades = await Trade.findAndCountAll({
-            order: [['status_updated', 'DESC']],
-            offset: req.body.offset,
-            limit: req.body.limit,
-            where: { [Op.and]: filters },
-            attributes: ['transaction_id', 'status_updated', 'rosters', 'managers', 'adds', 'drops', 'draft_picks', 'leagueLeagueId'],
-            include: [
-                {
-                    model: League,
-                    attributes: ['league_id', 'name', 'avatar', 'roster_positions', 'scoring_settings', 'settings'],
-                },
-                {
-                    model: User,
-                    attributes: [],
-                    through: { attributes: [] },
-                    include: {
-                        model: League,
-                        attributes: [],
-                        through: { attributes: [] },
-                        include: {
-                            model: User,
-                            attributes: [],
-                            through: { attributes: [] },
-                            where: {
-                                user_id: req.body.user_id
-                            },
-                            duplicating: false,
-                            subQuery: true
-                        },
-                        duplicating: false,
-                        required: true,
-                        subQuery: true
-
-                    },
-                    duplicating: false,
-                    required: true
-                }
-            ],
-            group: ['trade.transaction_id', 'league.league_id'],
-            raw: true
-        })
-
-        const trades_to_send = {
-            rows: lmTrades.rows,
-            count: lmTrades?.count?.length
-        }
-
-        res.send(trades_to_send)
-        
-    } catch (error) {
-        res.send(error)
-        console.log(error)
-    }
-
-
-
-}
+    res.send(trades_to_send);
+  } catch (error) {
+    res.send(error);
+    console.log(error);
+  }
+};
 
 exports.pricecheck = async (req, res) => {
+  let filters = [];
 
-    let filters = [];
+  if (req.body.player.includes(".")) {
+    const pick_split = req.body.player.split(" ");
+    const season = pick_split[0];
+    const round = parseInt(pick_split[1]?.split(".")[0]);
+    const order = parseInt(pick_split[1]?.split(".")[1]);
 
-    if (req.body.player.includes('.')) {
-        const pick_split = req.body.player.split(' ')
-        const season = pick_split[0]
-        const round = parseInt(pick_split[1]?.split('.')[0])
-        const order = parseInt(pick_split[1]?.split('.')[1])
+    filters.push({
+      price_check: {
+        [Op.contains]: [`${season} ${round}.${order}`],
+      },
+    });
+  } else {
+    filters.push({
+      price_check: {
+        [Op.contains]: [req.body.player],
+      },
+    });
+  }
 
-        filters.push({
-            price_check: {
-                [Op.contains]: [`${season} ${round}.${order}`]
-            }
-        })
+  if (req.body.player2) {
+    if (req.body.player2.includes(".")) {
+      const pick_split = req.body.player2.split(" ");
+      const season = pick_split[0];
+      const round = parseInt(pick_split[1]?.split(".")[0]);
+      const order = parseInt(pick_split[1]?.split(".")[1]);
+
+      filters.push({
+        players: {
+          [Op.contains]: [`${season} ${round}.${order}`],
+        },
+      });
     } else {
-        filters.push({
-            price_check: {
-                [Op.contains]: [req.body.player]
-            }
-
-        })
+      filters.push({
+        players: {
+          [Op.contains]: [req.body.player2],
+        },
+      });
     }
+  }
 
-    if (req.body.player2) {
-        if (req.body.player2.includes('.')) {
-            const pick_split = req.body.player2.split(' ')
-            const season = pick_split[0]
-            const round = parseInt(pick_split[1]?.split('.')[0])
-            const order = parseInt(pick_split[1]?.split('.')[1])
+  let pcTrades;
+  let players2;
 
-            filters.push({
-                players: {
-                    [Op.contains]: [`${season} ${round}.${order}`]
-                }
-            })
-        } else {
-            filters.push({
-                players: {
-                    [Op.contains]: [req.body.player2]
-                }
-            })
-        }
+  try {
+    pcTrades = await Trade.findAndCountAll({
+      order: [["status_updated", "DESC"]],
+      offset: req.body.offset,
+      limit: req.body.limit,
+      where: {
+        [Op.and]: filters,
+      },
+      attributes: [
+        "transaction_id",
+        "status_updated",
+        "rosters",
+        "managers",
+        "adds",
+        "drops",
+        "draft_picks",
+        "leagueLeagueId",
+      ],
+      include: {
+        model: League,
+        attributes: [
+          "name",
+          "avatar",
+          "scoring_settings",
+          "roster_positions",
+          "settings",
+        ],
+      },
+      raw: true,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 
-
-    }
-
-
-    let pcTrades;
-    let players2;
-
-    try {
-        pcTrades = await Trade.findAndCountAll({
-            order: [['status_updated', 'DESC']],
-            offset: req.body.offset,
-            limit: req.body.limit,
-            where: {
-                [Op.and]: filters
-            },
-            attributes: ['transaction_id', 'status_updated', 'rosters', 'managers', 'adds', 'drops', 'draft_picks', 'leagueLeagueId'],
-            include: {
-                model: League,
-                attributes: ['name', 'avatar', 'scoring_settings', 'roster_positions', 'settings']
-            },
-            raw: true
-        })
-
-
-    } catch (error) {
-        console.log(error)
-    }
-
-
-    res.send(pcTrades)
-
-}
+  res.send(pcTrades);
+};
